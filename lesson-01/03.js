3d232f7f9c74904eefaa8d0559feb3637a9cbf30
alexpodelinskii <nikitik00787@gmail.com>
2024-05-23 12:28:19 +0300

    feat: correct leesson 1 task 3 v2.4

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,20 +1,20 @@
 // code
 
 //task 2
-/*let number1 = Math.floor(Math.random() * 100);
+let number1 = Math.floor(Math.random() * 100);
 
-if (number1 % 2 === 1) {
+/*if (number1 % 2 === 1) {
     console.log(`[${number1}] :Это нечетное число`)
 } else {
     console.log(`[${number1}] :Это четное число`)
-}
-*/
+}*/
+
 //task 3
 let number = 1;
-console.log(1);
-console.log(3);
-console.log(3);
-console.log(1);
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
 
 //task 4
 /*
